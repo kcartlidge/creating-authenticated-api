7ec5fa8a9fa8227206e61a4e6c8f02abfaba88f8
K Cartlidge <code@kcartlidge.com>
2020-03-17 22:00:05 +0000

    Register JWT in the startup configuration

--- CreatingAuthenticatedApi/Startup.cs
+++ CreatingAuthenticatedApi/Startup.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
 
 namespace CreatingAuthenticatedApi
 {
@@ -18,6 +22,30 @@ namespace CreatingAuthenticatedApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["JWT:Key"])),
+
+                    ValidateIssuer = true,
+                    ValidIssuer = Configuration["JWT:Issuer"],
+
+                    ValidAudience = Configuration["JWT:Audience"],
+                    ValidateAudience = true,
+
+                    ValidateLifetime = true,
+                    RequireExpirationTime = true,
+                    ClockSkew = TimeSpan.FromSeconds(2),
+                };
+            });
             services.AddControllers();
         }
 
@@ -33,6 +61,7 @@ namespace CreatingAuthenticatedApi
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
